737e70b2491ec0e6edfc5a422dd48133c789a085
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-06-30 16:45:49 +0200

    ....

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,147 @@
-import './style.css'
+import "./style.css";
 // Using the provided emails and template files:
 // - Create all the state data you need to make your app work
 // - When the app loads, render a list of emails
 // - When a user clicks the email - render the page for that single email
 // - Once the email has been opened at least once - mark it as read
 // - See that search bar? Make it so when a user types something, you only display the emails who's sender's name contains that. (E.g. "nic" should only show Nico's email. "e" should show both Ed's and Government's emails. Take letter case into consideration, too)
-// - Try to make it work when inside the single email view as well! i.e. entering a new search term and hitting enter should take you back to the email list view and show only the emails that match the filter. 
+// - Try to make it work when inside the single email view as well! i.e. entering a new search term and hitting enter should take you back to the email list view and show only the emails that match the filter.
 
 // Bonus
 // - We know enough to add our own features. Let's add buttons to delete email, create new emails, etc 🙂
 
-const state = {
+type Email = {
+  from: string;
+  header: string;
+  content: string;
+  emailAddress: string;
+  img: string;
+  read: boolean;
+};
+type State = {
+  emails: Email[];
+};
+const state: State = {
   emails: [
     {
-      from: 'Nico',
+      from: "Nico",
       header: "Link to today's video and slides is up!",
       content:
-        'Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
-      emailAddress: 'nico@email.com',
-      img: 'assets/nico.JPG',
-      read: false
+        "Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "nico@email.com",
+      img: "assets/nico.JPG",
+      read: true,
     },
     {
-      from: 'Ed',
+      from: "Ed",
       header:
         "Congratulations! You have received a free beaver! Your name will now be displayed in the classroom's beaver list!",
       content:
-        'Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
-      emailAddress: 'ed@email.com',
-      img: 'assets/ed.JPG',
-      read: false
+        "Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "ed@email.com",
+      img: "assets/ed.JPG",
+      read: false,
     },
     {
-      from: 'Government',
-      header: 'Time to pay your tax!',
+      from: "Government",
+      header: "Time to pay your tax!",
       content:
-        'Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
-      emailAddress: 'government@email.com',
-      img: 'assets/gov.jpg',
-      read: false
-    }
+        "Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "government@email.com",
+      img: "assets/gov.jpg",
+      read: false,
+    },
     // feel free to add more emails here
-  ]
+  ],
+};
+function renderEmailListItem(email: Email, emailListEl: HTMLUListElement) {
+  let liEl = document.createElement("li");
+  liEl.className = email.read ? "emails-list__item read" : "emails-list__item";
+  let iconSpanEl = document.createElement("span");
+  iconSpanEl.className =
+    "emails-list__item__read-icon material-symbols-outlined";
+  iconSpanEl.textContent = email.read ? "mark_email_read" : "mark_email_unread";
+  let imgEl = document.createElement("img");
+  imgEl.className = "emails-list__item__image";
+  imgEl.src = email.img;
+  let pEl = document.createElement("p");
+  pEl.className = "emails-list__item__from";
+  pEl.textContent = email.from;
+  let pEl2 = document.createElement("p");
+  pEl2.className = "emails-list__item__content";
+  pEl2.textContent = email.content;
+
+  emailListEl.append(liEl);
+  liEl.append(iconSpanEl, imgEl, pEl, pEl2);
 }
+
+function renderEmailList() {
+  let mainEl = document.querySelector("main");
+  if (mainEl === null) return;
+  mainEl.textContent = "";
+
+  let titleEl = document.createElement("h1");
+  titleEl.textContent = "Inbox";
+  let emailListEl = document.createElement("ul");
+  emailListEl.className = "emails-list";
+
+  for (let email of state.emails) {
+    renderEmailListItem(email, emailListEl);
+  }
+
+  mainEl.append(titleEl, emailListEl);
+}
+function renderSingleEmail(email: Email, sectionEl: HTMLElement) {}
+function renderEmailDetails() {
+  let mainEl = document.querySelector("main");
+  if (mainEl === null) return;
+  mainEl.textContent = "";
+
+  let sectionEl = document.createElement("section");
+  sectionEl.className = "single-email";
+
+
+  for (let email of state.emails) {
+    let backButtonEl = document.createElement("button");
+    backButtonEl.className = "single-email__button";
+    backButtonEl.textContent = "⬅Back";
+    let divEl = document.createElement("div");
+    divEl.className = "single-email__sender-section";
+    let imgEl = document.createElement("img");
+    imgEl.className = "single-email__image";
+    imgEl.src = email.img;
+    let spanEl = document.createElement("span");
+    spanEl.className = "single-email__sender";
+    (spanEl.textContent = email.from), email.emailAddress;
+    let titleEl = document.createElement("h1");
+    titleEl.className = "single-email__header";
+    titleEl.textContent = email.header;
+    let contentEl = document.createElement("p");
+    contentEl.className = "single-email__content";
+    contentEl.textContent = email.content;
+
+    sectionEl.append(backButtonEl, divEl, titleEl, contentEl);
+    divEl.append(imgEl, spanEl);
+  }
+  mainEl.append(sectionEl);
+
+  //   <section class="single-email">
+  //   <button class="single-email__button">⬅Back</button>
+  //   <div class="single-email__sender-section">
+  //     <img class="single-email__image" src="assets/nico.JPG" />
+  //     <span class="single-email__sender">Nico (nico@email.com)</span>
+  //   </div>
+  //   <h1 class="single-email__header">Link to today's video and slides is up!</h1>
+  //   <p class="single-email__content">
+  //     Link is up and you know where to find it! Lorem ipsum dolor sit amet
+  //     consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis
+  //     incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a
+  //     architecto earum voluptatibus vel atque.
+  //   </p>
+  // </section>
+}
+function render() { 
+  renderEmailList();
+  renderEmailDetails();
+}
+render();
